c3fa7c350f2356a5172f4d6856f7440c509a4eb0
shamita <shamita.kandru@gmail.com>
2020-01-22 10:00:57 -0500

    First push of Lab 2 - Room Calculator

---
+++ Lab 2 - Room Calculator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lab_2___Room_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double roomLength, roomWidth;
+            char userResponse = 'y';
+
+            while (userResponse == 'y')
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Enter Length: ");
+                roomLength = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Width: ");
+                roomWidth = double.Parse(Console.ReadLine());
+
+                Console.Write("Enter Height: ");
+                double roomHeight = double.Parse(Console.ReadLine());
+
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write($"\nEnter Length: {roomLength} \nEnter Width: {roomWidth} \nEnter Height: {roomHeight}\n");
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                double roomArea = (roomLength * roomWidth);
+                Console.WriteLine($"\nArea: {roomArea}");
+
+                double roomPerimeter = (2 * (roomLength + roomWidth));
+                Console.WriteLine($"Perimeter: {roomPerimeter}");
+
+                double roomVolume = (roomLength * roomWidth * roomHeight);
+                Console.WriteLine($"Volume: {roomVolume}"); ;
+
+
+                userResponse = ' ';
+
+                while ((userResponse != 'y') && (userResponse != 'n'))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("\nContinue? (y/n) ");
+                    userResponse = char.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Welcome Back!\n");
+                }
+
+            }
+
+
+            Console.WriteLine("Okie Dokie. Thank you for your time!");
+
+            Console.ReadLine();
+
+
+       
+
+       
+            
+        }
+
+    }
+}
