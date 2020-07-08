3c55893fca09f48d7aca73f9cbece49eaf7fec18
shamita <shamita.kandru@gmail.com>
2020-07-08 18:49:50 -0400

    Redid everything, inserted seperate methods for calculations.

--- Lab 2 - Room Calculator/Program.cs
+++ Lab 2 - Room Calculator/Program.cs
@@ -6,60 +6,114 @@ namespace Lab_2___Room_Calculator
     {
         static void Main(string[] args)
         {
-            double roomLength, roomWidth;
-            char userResponse = 'y';
-
-            while (userResponse == 'y')
+            Console.ForegroundColor = ConsoleColor.White;
+            //Objective: Calculate the perimeter and area of the classroom based on user's input
+            Console.WriteLine("Welcome to the Room Calculator!" +
+                "\nPlease enter your answer in feet!\n");
+            do
             {
+
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Enter Length: ");
-                roomLength = double.Parse(Console.ReadLine());
+                GetUserInput("\nEnter Length: ");
+                double length = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter Width: ");
-                roomWidth = double.Parse(Console.ReadLine());
+                GetUserInput("Enter Width ");
+                double width = double.Parse(Console.ReadLine());
 
-                Console.Write("Enter Height: ");
-                double roomHeight = double.Parse(Console.ReadLine());
+                //The height is also calculated to get the volume but is asked in the GetVolume Method only if they would like to proceed.
+                //The reason why I did this is because I didn't want the user to input information if they didn't want to.
+                GetVolume(length, width);
 
+                //Calculations in GetArea Method
+                GetArea(length, width);
 
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write($"\nEnter Length: {roomLength} \nEnter Width: {roomWidth} \nEnter Height: {roomHeight}\n");
+                //Calculations in GetPerimeter Method
+                GetPerimeter(length, width);
+
+            } while (Continue());
+
+        }
+
+        static string GetUserInput(string userResponse)
+        {
+            //created a seperate method to get any input. 
+            Console.WriteLine(userResponse);
+            return userResponse;
+        }
+
+        static void GetArea(double length, double width)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            //area = length * width
+            double area = length * width;
+            Console.WriteLine($"Area: {area} feet squared");
+        }
+
+        static void GetPerimeter(double length, double width)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            //perimeter = 2 * (length + width)
+            double perimeter = 2 * (length + width);
+            Console.WriteLine($"Perimeter: {perimeter} feet squared\n");
+            Console.ReadLine();
+
+        }
+
+        static void GetVolume(double width, double length)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            //the user only inputs the height if they want to continue.
+            GetUserInput("\nWould you also like to get the volume of your room? Enter y/n: ");
+            string userResponse = Console.ReadLine().ToLower();
+
+            if (userResponse == "yes" || userResponse == "y")
+            {
+                GetUserInput("Enter Height: ");
+                double height = double.Parse(Console.ReadLine().ToLower());
+                double volume = length * width * height;
 
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                double roomArea = (roomLength * roomWidth);
-                Console.WriteLine($"\nArea: {roomArea}");
-
-                double roomPerimeter = (2 * (roomLength + roomWidth));
-                Console.WriteLine($"Perimeter: {roomPerimeter}");
-
-                double roomVolume = (roomLength * roomWidth * roomHeight);
-                Console.WriteLine($"Volume: {roomVolume}"); ;
-
-
-                userResponse = ' ';
-
-                while ((userResponse != 'y') && (userResponse != 'n'))
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("\nContinue? (y/n) ");
-                    userResponse = char.Parse(Console.ReadLine());
-
-                    Console.WriteLine("Welcome Back!\n");
-                }
-
+                Console.WriteLine($"\nVolume: {volume} feet cubed");
+            }
+            else if (userResponse == "no" || userResponse == "n")
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Error! Please enter either 'y' or 'n': ");
             }
 
 
-            Console.WriteLine("Okie Dokie. Thank you for your time!");
+        }
+        static bool Continue()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            GetUserInput("Would you like to continue? Please enter y/n: ");
+            string userContinue = Console.ReadLine().ToLower();
 
-            Console.ReadLine();
+            if (userContinue == "yes" || userContinue == "y")
+            {
+                Console.WriteLine("Awesomesauce! \n");
+                return true;
+            }
+            else if (userContinue == "no" || userContinue == "n")
+            {
+                Console.WriteLine("Thanks for your input, have a nice day!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Error! Please answer with a either y/n: ");
+                Continue();
+            }
 
-
-       
-
-       
-            
+            return true;
         }
 
     }
+
 }
+
